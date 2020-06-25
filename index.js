a3fead41992443b95818de93aa08ea38db51c853
Daniel <daniel.d@spot.im>
2020-06-25 15:52:34 +0300

    prettier

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
-const express=require('express');
-const fetch = require('node-fetch');
-const ejs = require('ejs');
+const express = require("express");
+const fetch = require("node-fetch");
+const ejs = require("ejs");
 const app = express();
-app.listen(8081,function(){
-	console.log("Express is running on port 8081");
+app.listen(8081, function () {
+  console.log("Express is running on port 8081");
 });
 
-app.set('view engine', ejs);
+app.set("view engine", ejs);
 
-const asyncMiddleware = fn =>
-  (req, res, next) => {
-    Promise.resolve(fn(req, res, next))
-      .catch(next);
-  };
+const asyncMiddleware = (fn) => (req, res, next) => {
+  Promise.resolve(fn(req, res, next)).catch(next);
+};
 
-app.get('/',asyncMiddleware(async (req,res, next)=> {
-    const convResponse =  await fetch('https://extractify.spot.im/conversation/sp_ly3RvXf6/p0st2');
+app.get(
+  "/",
+  asyncMiddleware(async (req, res, next) => {
+    const convResponse = await fetch(
+      "https://extractify.spot.im/conversation/sp_ly3RvXf6/p0st2"
+    );
     const convHtml = await convResponse.text();
 
-	res.render('pages/html.ejs', {body: convHtml})
-
-}));
+    res.render("pages/html.ejs", { body: convHtml });
+  })
+);
