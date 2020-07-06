a6243a39dd18807367b7d364030f46d8093c2b38
Daniel <daniel.d@spot.im>
2020-07-06 12:26:36 +0300

    final

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const fetch = require("node-fetch");
+const zlib = require("zlib");
 const ejs = require("ejs");
 const app = express();
+
 app.listen(process.env.PORT || 8081, function () {
   console.log("Express is running on port 8081");
 });
@@ -12,16 +14,34 @@ const asyncMiddleware = (fn) => (req, res, next) => {
   Promise.resolve(fn(req, res, next)).catch(next);
 };
 
+
 app.get(
   "/",
   asyncMiddleware(async (req, res, next) => {
+    const { spotId, postId} = req.query;
     res.set('Content-Encoding', 'gzip');
-    res.set('Accept-Encoding', 'gzip')
+    res.set('Accept-Encoding', 'gzip');
     res.set('Content-type', 'text/html; charset=utf-8');
     const convResponse = await fetch(
-      "http://extractify.spot.im/conversation/sp_IjnMf2Jd/24540557"
+`https://extractify.spot.im/conversation/${spotId}/${postId}`
     );
     const convHtml = await convResponse.text();
-    res.render("pages/html.ejs", { body: convHtml });
+    const wrappedHtml = `<!DOCTYPE html>
+    <html lang="en" dir="ltr">
+    
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport"
+        content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <title>Demo SpotIM SEO Conversation</title>
+    
+    <body>
+      ${convHtml}
+    </body>
+    
+    </html>`
+    const zipped = zlib.gzipSync(Buffer.from(wrappedHtml, 'utf-8'));
+    res.status(200).end(zipped)
   })
 );
